55fd4277f003e39fa6e479f873c9006d388c3819
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-09-06 16:15:53 +0100

    Query String

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -10,12 +10,50 @@ app.get("/", (req, res) => {
   res.send('<h1>Home page</h1><a href="api/products">products</a>');
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send(`<h1>Resource not found</h1>`);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
+
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req.params)
+  const {productID} = req.params;
+  const singleProduct= products.find((product)=> product.id === Number(productID))
+  if(!singleProduct){
+    return res.status(404).send('Product does not exist!')
+  }
+  return res.json(singleProduct);
+});
+
+app.get(`/api/v1/query`, (req,res)=>{
+  // console.log(req.query)
+  const {search, limit} = req.query
+  let sortedProducts = [...products]
+
+  if (search){
+    sortedProducts = sortedProducts.filter((product)=>{
+      return product.name.startsWith(search)
+    })
+  }
+  if(limit){
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if(sortedProducts < 1){
+    // res.status(200).send('no products matched your search')
+    return res.status(200).json({success: true, data: [] })
+  }
+  return res.status(200).json(sortedProducts)
+})
+
+// app.all("*", (req, res) => {
+//   res.status(404).send(`<h1>Resource not found</h1>`);
+// });
 
 app.listen(3500, () => {
   console.log("server listening on port 5000");
 });
 
-//5:35/12
+//6:01:33
